48a91bd6997da439709fee34c3ea8477e5aa77af
AbdoNJ <mr.nejjari.abdelhafid@gmail.com>
2023-03-11 01:47:33 +0100

    Add local db config file and user model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 2 }
+  end
+  
--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,17 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RorRecipesFood
+module Food
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
